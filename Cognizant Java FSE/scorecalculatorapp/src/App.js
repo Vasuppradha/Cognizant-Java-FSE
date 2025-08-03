86c9b9f9dbd80755c380c92ff7ef1e81b9f21836
Vasuppradha <vasuppradha0310@gmail.com>
2025-08-03 23:49:08 +0530

    Add files via upload

---
+++ Cognizant Java FSE/scorecalculatorapp/src/App.js
@@ -0,0 +1,14 @@
+import { CalculateScore } from "./Components/CalculateScore";
+
+function App(){
+  return(
+    <div>
+      <CalculateScore Name={"Vasu"}
+      School={"Oxford"}
+      total = {300}
+      goal={3}/>
+    </div>
+  )
+}
+
+export default App;
