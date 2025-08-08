bfa64a6dfe609981e03008aaddec4a93e4e56953
Vasuppradha <vasuppradha0310@gmail.com>
2025-08-08 15:11:36 +0530

    Add files via upload

---
+++ Cognizant Java FSE/Week 7 Exercises/ticketbookingapp/src/components/LoginButton.js
@@ -0,0 +1,5 @@
+import React from 'react';
+function LoginButton({ onClick }) {
+    return <button onClick={onClick}>Login</button>;
+}
+export default LoginButton;
---
+++ Cognizant Java FSE/Week 7 Exercises/ticketbookingapp/src/components/LogoutButton.js
@@ -0,0 +1,5 @@
+import React from 'react';
+function LogoutButton({ onClick }) {
+    return <button onClick={onClick}>Logout</button>;
+}
+export default LogoutButton;
